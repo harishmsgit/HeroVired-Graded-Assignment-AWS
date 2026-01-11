e09344c29be76ec6c125b19c8c5b6059fe75824f
harishmsgit <senharishms108@gmail.com>
2026-01-11 23:05:32 +0530

    fix npm start error and fixed

--- backend/conn.js
+++ backend/conn.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const URL = process.env.MONGO_URI
+// const URL = process.env.MONGO_URI
+const URL = "mongodb://localhost:27017/TravelMemory"
 
 mongoose.connect(URL)
 mongoose.Promise = global.Promise
